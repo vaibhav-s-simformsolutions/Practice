df57e81b576271ee7d57595c76f2544bc4f8c0f2
vaibhav-s-simformsolutions <vaibhav.s@simformsolutions.com>
2021-01-13 00:02:52 +0530

    TypeScript Added

---
+++ TypeScipt/src/topBottom.ts
@@ -0,0 +1,10 @@
+//bottom types
+let bottomvar : number | string = 10;
+if(typeof bottomvar == 'string'){
+    //for string
+}else if(typeof bottomvar == 'number'){
+    //for number
+}else{
+    //for default
+    console.log(bottomvar);
+}
